72253f86f3df4deb2787852d74ecee6f00cd6fa8
Henry <hnr.leto11@gmail.com>
2020-10-07 09:32:59 -0400

    init commit blogger app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,10 @@
+# responsible for article controls
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
